ea3a0b7b08054f35056b1e880a24da26c30bfcfd
IlluzionzDev <jaminstratford@gmail.com>
2021-09-13 20:11:03 +1200

    Setup Svelte

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import logo from './svelte-logo.svg';
+</script>
+
+<header>
+	<div class="corner">
+		<a href="https://kit.svelte.dev">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+</header>
+
+<style lang="scss">
+	@import '../../css/colors.scss';
+
+	header {
+		display: flex;
+		justify-content: space-between;
+		background-color: $primary-blue;
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../css/global.scss';
+	import Header from '$lib/header/Header.svelte';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+</footer>
