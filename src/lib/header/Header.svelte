b40cb7b1e3125583b75010507f3302688a8ead18
IlluzionzDev <jaminstratford@gmail.com>
2021-09-15 23:40:33 +1200

    Basic header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,22 +1,71 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
+	import background from './background.svg';
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
+	<img src="{background}" alt="Background" class="header-background"> 
+	<div class="container">
+		<nav>
+			<ul>
+				<li class="brand">
+					<h1><a href="/">Computron</a></h1>
+				</li>
+				<li><a href="/">Home</a></li>
+				<li><a href="/software">Software</a></li>
+				<li><a href="/tutorials">Tutorials</a></li>
+			</ul>
+		</nav>
 	</div>
 </header>
 
+<!-- Css specific to this component -->
 <style lang="scss">
 	@import '../../css/colors.scss';
 
 	header {
 		display: flex;
 		justify-content: space-between;
-		background-color: $primary-blue;
+		align-items: center;
+	}
+
+	.header-background {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		z-index: -999;
+	}
+
+	// Branding on nav
+	.brand {
+		font-weight: bold;
+	}
+
+	nav {
+		ul {
+			display: flex;
+			justify-content: flex-end;
+			align-items: center;
+			gap: 2rem;
+			margin: 0;
+			padding: 0;
+
+			li {
+				list-style: none;
+				font-size: 18px;
+				position: relative;
+				padding: 1.5rem 1rem;
+
+				a {
+					color: $white;
+					text-decoration: none;
+				}
+
+				// Position first element on left
+				&:first-child {
+					margin-right: auto;
+				}
+			}
+		}
 	}
 </style>
