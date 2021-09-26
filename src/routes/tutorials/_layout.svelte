4a14299b185b4bbdf8751238a3a9c528c64fa8b4
IlluzionzDev <jaminstratford@gmail.com>
2021-09-27 11:07:40 +1300

    Fix scss compiling for markdown

--- src/routes/tutorials/_layout.svelte
+++ src/routes/tutorials/_layout.svelte
@@ -1,6 +1,5 @@
 <script>
 	import Hero from '$lib/hero/Hero.svelte';
-	import { base } from '$app/paths';
 
 	export let title;
 	export let description;
@@ -16,44 +15,13 @@
 		<h1>{title}</h1>
 		<p>{description}</p>
 		<div class="button-row">
-			<a href="{base}/tutorials"><button class="btn btn-solid">GO BACK</button></a>
+			<a href="/tutorials"><button class="btn btn-solid">GO BACK</button></a>
 		</div>
 	</Hero>
 
 	<div class="tutorial">
 		<!-- Tutorial content -->
+		<!-- Due to the nature of markdown, have to have scss in an external file -->
 		<slot />
 	</div>
 </div>
-
-<style lang="scss">
-	@import '../../css/colors.scss';
-
-	// Format the markdown container
-	.tutorial {
-		padding-bottom: 4rem;
-		text-align: center;
-
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-	}
-
-	// Formatting for the actual markdown elements
-	.tutorial :global(h1) {
-		font-size: 3rem;
-
-		padding: 22px;
-		padding-top: 8.875rem;
-
-		&:first-child {
-			padding-top: 0;
-		}
-	}
-
-	.tutorial :global(p) {
-		font-size: 1.125rem;
-		width: 80%;
-	}
-</style>
